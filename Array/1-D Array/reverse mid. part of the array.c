67ac695847d2d2e02bf4e725ee57d17524aee365
Vikas Yadav <yvikas2006@gmail.com>
2025-04-01 08:48:12 +0530

    Create reverse mid. part of the array.c

---
+++ Array/1-D Array/reverse mid. part of the array.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+    int a;
+    scanf("%d",&a);
+    int arr[a];
+    for(int i=0;i<a;i++){
+        scanf("%d",&arr[i]);
+    }
+    for(int i=1,j=4;i<=j;i++,j--){
+            int temp=arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;
+        }
+    
+    for(int i=0;i<a;i++){
+        printf("%d ",arr[i]);
+    }
+
+}
+        
+    
+    
