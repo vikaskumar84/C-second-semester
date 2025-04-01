2731b7fa0bf06d51881fd2627a888da4baf1b5ee
Vikas Yadav <yvikas2006@gmail.com>
2025-04-01 08:56:07 +0530

    Update Reversing From middle index of an Array.c

--- Array/1-D Array/Functions with Array/Reversing From middle index of an Array.c
+++ Array/1-D Array/Functions with Array/Reversing From middle index of an Array.c
@@ -1,1 +1,28 @@
 
+#include<stdio.h>
+int reverse(int arr[],int a,int b){
+    for(int i=a,j=b;i<=j;i++,j--){
+        int temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+    }
+    return 0;
+}
+int main(){
+    int a;
+    scanf("%d",&a);
+    int arr[a];
+    for(int i=0;i<a;i++){
+        scanf("%d",&arr[i]);
+    }
+    reverse(arr,1,4);
+    
+    
+    for(int i=0;i<a;i++){
+        printf("%d ",arr[i]);
+    }
+
+}
+        
+    
+    
