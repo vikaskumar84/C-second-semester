452290bbfbf18d0bccbebf4e754330630b251b79
Vikas Yadav <yvikas2006@gmail.com>
2025-05-13 16:05:32 +0530

    Update searching elements in the array.c

--- Array/1-D Array/searching elements in the array.c
+++ Array/1-D Array/searching elements in the array.c
@@ -1,23 +1,22 @@
 #include<stdio.h>
 int main(){
-    int a; 
+    int a, f=0; 
     scanf("%d",&a);
     int arr[a],x=12;
     for(int i=0;i<a;i++){
         scanf("%d",&arr[i]);
     }
-    int f=0;
     for(int i=0;i<a;i++){
             if(arr[i]==x){
                 f=1;
                 break;
             }
     }
-            if(f==0){
-                printf("%d is not present in the array",x);
+            if(f==1){
+                printf("%d is present in the array",x);
             }
     else{
-        printf("%d is present in the array",x);
+        printf("%d is not present in the array",x);
     }
 }
 
