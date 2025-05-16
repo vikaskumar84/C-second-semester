c0500a62a6af9b4c07bf280615dc0da54ccfcbf7
Vikas Yadav <yvikas2006@gmail.com>
2025-05-16 12:25:50 +0530

    Update remove duplicate elements in the array.c

--- Array/1-D Array/remove duplicate elements in the array.c
+++ Array/1-D Array/remove duplicate elements in the array.c
@@ -1,31 +1,44 @@
-#include <stdio.h>
-int main(){
-    int a,f=0;
-    scanf("%d",&a);
-    int arr[a];
-    for(int i=0;i<a;i++){
+#include<stdio.h>
+int main()
+{
+    int n ,count=0;
+    scanf("%d",&n);
+    int arr[n];
+    int brr[n];
+    int new_arr[n];
+    for(int i=0;i<n;i++){
         scanf("%d",&arr[i]);
     }
-    for(int i=0;i<a;i++){
-        f=0;
-        for(int j=0;j<i;j++){
-            if(arr[i]==arr[j]){
-                f=1;
-                break;
+    for(int i=0;i<n;i++){
+        brr[i]=arr[i];
+    }
+
+    for (int i  = 0 ; i <n ; i++)
+    {
+        int f=0;
+        for (int j = i+1 ; j <n ; j++)
+        {
+            if(arr[i]==brr[j])
+            {
+                count+=1;
+                continue;
+                
+            }
+            else{
+                new_arr[i]=arr[i];
+
             }
         }
-        if(!f){
-            printf("%d ",arr[i]);
-        }
-
     }
+    printf("old arrray\n");
+    for(int i=0;i<n;i++){
+        printf("%d ",arr[i]);
+    }
+    printf("\n");
+
+    printf("new array\n");
     
+    for(int i=0;i<n-count;i++){
+        printf("%d ",new_arr[i]);
+    }
 }
-
-
-
-
-
-
-    
-   
